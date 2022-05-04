764cf0a46a21bc6479d25505ff3e0c5486426871
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-05-04 23:18:28 +0200

    CRUD_users____ProblemsWithJwt

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,63 @@
 const router = require("express").Router();
-const {verifyJwtToken} = require("./verifyJwtToken");
+const {verifyJwtToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyJwtToken");
 
-router.put("/:id", verifyJwtToken, (req, res)=>{
-    
-})
 
+
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASSWORD_SECRET
+      ).toString();
+    }
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+//Delete
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted.");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //Get user
+  router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  //Get all users
+  router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 module.exports = router
--- routes/verifyJwtToken.js
+++ routes/verifyJwtToken.js
@@ -1,16 +1,47 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 
 const verifyJwtToken = (req,res, next)=>{
-    const authHeader = req.headers.token
-    if(authHeader){
-        jwt.verify(token, process.env.JWT_SECRET, (error, data)=>{
-            if(error) res.status(401).json("Invalid token.");
-            req.user = user;
-            next();
-        })
-    }else{
-        return res.status(401).json("Unauthorized user.");
-    }
+    const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+      if (error) {
+          res.status(403).json("Token is not valid!");
+      }
+      req.user = user;
+      
+      next();
+      
+    });
+   
+  } else {
+  
+    return res.status(401).json("Unauthorized user.");
+
+  }
 }
-module.exports={verifyJwtToken};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyJwtToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("ou are not authorized to do that!");
+      }
+    });
+  };
+
+  const verifyTokenAndAdmin = (req, res, next) => {
+    verifyJwtToken(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        res.status(403).json("You are not alowed to do that!");
+      }
+    });
+  };
+
+  
+
+module.exports={verifyJwtToken, verifyTokenAndAuthorization, verifyTokenAndAdmin};
